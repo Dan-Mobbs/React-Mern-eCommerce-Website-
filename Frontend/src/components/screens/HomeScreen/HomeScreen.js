200191aae3ca318728da07f4aacb186ad757030c
Daniel <danmobbs.101@gmail.com>
2021-04-24 21:41:54 +0100

    proejct UI

--- Frontend/src/components/screens/HomeScreen/HomeScreen.js
+++ Frontend/src/components/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,54 @@
-import './HomeScreen.css'
+import './HomeScreen.css';
+import { Card, CardDeck, Container } from 'react-bootstrap';
 
 const HomeScreen = () => {
     return (
-        <div className="homescreen">
-            <h1>HomeScreen</h1>
-        </div>
+        <>
+        <Container className="py-5">
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional
+                        content.{' '}
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to
+                        show that equal height action.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
+        </Container>    
+        </>
     )
 }
 
